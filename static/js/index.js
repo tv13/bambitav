7a4b758cedf34ec575ba905428423a2ce160aaad
Eugene Oleynyak <sarenos@gmail.com>
2016-01-20 12:10:53 +0200

    cfdfdf

--- static/js/index.js
+++ static/js/index.js
@@ -8,17 +8,16 @@ $(document).ready(function(){
 
         },
         success: function(data){
-      var dataJson = $.parseJSON(data);
       var maxLenth = 10;
-      for ( var i = 0, l = dataJson.data.length; i < l; i++ ) {
+      for ( var i = 0, l = data.data.length; i < l; i++ ) {
         var string = "";
         var  string2 = string.concat("<div class=\"row\">", "<div class=\"col-md-6 portfolio-item\">");
         var  string3 = string2.concat("<a href=\"#\">", "<img class=\"img-responsive\" src=\"http://placehold.it/700x400\" alt=\"\">",
-          "</a>", "<h3>", "<a href=\"#\">",dataJson.data[i].id,", " ,dataJson.data[i].name , ", ",dataJson.data[i].birthdate,"</a>", "</h3>", "</div>");
+          "</a>", "<h3>", "<a href=\"#\">",data.data[i].id,", " ,data.data[i].name , ", ",data.data[i].birthdate,"</a>", "</h3>", "</div>");
           i++;
           if(i < l){
          var string4 = string3.concat("<div class=\"col-md-6 portfolio-item\">", "<a href=\"#\">", "<img class=\"img-responsive\" src=\"http://placehold.it/700x400\" alt=\"\">", "</a>", "<h3>",
-          "<a href=\"#\">",dataJson.data[i].id,", ",dataJson.data[i].name ,", ",dataJson.data[i].birthdate,"</a></h3>", "</div>");
+          "<a href=\"#\">",data.data[i].id,", ",data.data[i].name ,", ",data.data[i].birthdate,"</a></h3>", "</div>");
                       var string5 = string4.concat("</div>");
           }
           else
