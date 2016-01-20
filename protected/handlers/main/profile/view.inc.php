419da92e9b7c8042ec72f7449433d2d8d8abf010
akuznetsova <a.kuznetsova@webinerds.com>
2016-01-20 21:56:42 +0200

    update carousel

--- protected/handlers/main/profile/view.inc.php
+++ protected/handlers/main/profile/view.inc.php
@@ -12,5 +12,6 @@ class MainProfileView extends ListView
         parent::fill();
         $this->set_template('profile/profile.tpl');
         $this->assign('balance_value', $this->Model->get_balance());
+        $this->assign('images', array('https://i.onthe.io/wjfkb82tfgre6ich7.91d8cbb5.jpg', 'https://i.onthe.io/wjfkb87ulito8hst8.3cc55de8.jpg'));
     }
 };
